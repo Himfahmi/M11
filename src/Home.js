b579d6ca3a8664dc2716319bdcdbc2435e82eebe
Himfahmi <you@example.com>
2024-01-05 14:44:21 +0700

    update .gitignore

--- src/Home.js
+++ src/Home.js
@@ -48,13 +48,13 @@ function Homepage() {
           <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
               <div className="carousel-item active">
-                <img src="/produk-1.jpg" className="d-block w-100" alt="..." />
+                <img src="./produk-1.jpg" className="d-block w-100" alt="..." />
               </div>
               <div className="carousel-item">
-                <img src="/produk-2.jpg" className="d-block w-100" alt="..." />
+                <img src="./produk-2.jpg" className="d-block w-100" alt="..." />
               </div>
               <div className="carousel-item">
-                <img src="/produk-3.jpg" className="d-block w-100" alt="..." />
+                <img src="./produk-3.jpg" className="d-block w-100" alt="..." />
               </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
